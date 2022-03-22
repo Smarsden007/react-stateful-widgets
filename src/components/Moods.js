6a17f0f61b2d9e1dc3d338a2bc70e5f8fb5e3b8c
Sean Marsden <sean@Seans-MacBook-Pro.local>
2022-03-21 21:43:26 -0700

    mood widget is complete

--- src/components/Moods.js
+++ src/components/Moods.js
@@ -40,12 +40,15 @@ export default function Moods() {
 const [mood, setMood] = useState(happyMood);
 
   const makeHappy = () => {
+    setMood(happyMood);
     /* STEP 4 */
   };
   const makeSad = () => {
+    setMood(sadMood)
     /* STEP 5 */
   };
   const reset = () => {
+    setMood(initialMood)
     /* STEP 6 */
   };
 
@@ -58,7 +61,7 @@ const [mood, setMood] = useState(happyMood);
   return (
     <div className='widget-moods container'>
       <h2>Moods</h2>
-      <div id='mood' style={style}>Not sure how I feel</div> {/* STEP 3 */}
+      <div id='mood' style={style}>{mood}</div> {/* STEP 3 */}
       <div>
         <button id='makeHappy' onClick={makeHappy}>Make Happy</button>
         <button id='makeSad' onClick={makeSad}>Make Sad</button>
