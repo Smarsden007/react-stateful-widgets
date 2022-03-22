d6fefdb664f4bde2d4849e6476299dbec1bfe7d3
Sean Marsden <sean@Seans-MacBook-Pro.local>
2022-03-21 21:40:46 -0700

    mood widget is almost complete

--- src/components/Moods.js
+++ src/components/Moods.js
@@ -28,14 +28,16 @@ STEPS 4, 5, 6:
   Inside these click handlers set the correct mood, using 'setMood' and the variables below the imports.
 */
 
-import React from 'react'; /* STEP 0 */
+import React, { useState } from 'react'; /* STEP 0   Complete*/
 
 const initialMood = 'Not sure how I feel';
 const happyMood = 'Quite happy!';
 const sadMood = 'Rather sad';
 
 export default function Moods() {
+  
   /* STEP 1 */
+const [mood, setMood] = useState(happyMood);
 
   const makeHappy = () => {
     /* STEP 4 */
@@ -50,7 +52,7 @@ export default function Moods() {
   const style = {
     fontSize: '1.5em',
     marginBottom: '0.3em',
-    color: 'crimson', /* STEP 2 */
+    color: mood === happyMood ? 'royalblue' : 'crimson', /* STEP 2 */
   };
 
   return (
